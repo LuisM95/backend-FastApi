e6bfe6a2a6940a4aaa935a49d9ea3a95eac05fa4
Luis Martel <martel19.lm@gmail.com>
2023-11-18 01:29:22 +0100

    Update client.py

--- db/client.py
+++ db/client.py
@@ -5,4 +5,4 @@ from pymongo import MongoClient
 
 """Base de Datos local """
 
-db_client = MongoClient("mongodb+srv://Luigino95:The_rg95!$@cluster0.5bw287q.mongodb.net/?retryWrites=true&w=majority").test
+db_client = MongoClient("mongodb+srv://Username:<password>@cluster0.5bw287q.mongodb.net/?retryWrites=true&w=majority").test
